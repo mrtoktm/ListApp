99d0a251d743af17102d4b4d4c55add45068695b
mrtoktm <mrtoktm9@gmail.com>
2024-03-05 13:34:01 +0300

    Initial Commit

---
+++ ListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListApp
+//
+//  Created by Mert Öktem on 5.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
